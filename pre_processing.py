204cb4cb9be955c3df92c4d1729668ded14a5d94
Michael Schroeder <mikejsch@gmail.com>
2025-02-20 22:48:42 -0500

    add non-working multi-pre-processing

--- pre_processing.py
+++ pre_processing.py
@@ -1,12 +1,13 @@
 import logging
 import re
+import time
 from typing import List
 
 import pandas as pd
 from pypdf import PdfReader
 
 logger = logging.getLogger(__name__)
-log_name = 'preprocess_log'
+log_name = 'preprocess_log.log'
 logging.basicConfig(encoding='utf-8', 
                     level=logging.INFO,
                     handlers=[
@@ -14,13 +15,16 @@ logging.basicConfig(encoding='utf-8',
                         #logging.StreamHandler()
                     ])
 
+csv_path = "boston_marathon.csv" 
+pdf_path = "BostonMarathonHistoricalResults.pdf"
+
 def clean_spaces(phrases: List[str]):
     clean_phrases = [re.sub(' ([A-Z])', '||||\\1', phrase).replace(' ', '').replace('||||', ' ') for phrase in phrases]
     return clean_phrases
 
-pdf_path = "BostonMarathonHistoricalResults.pdf"
 reader = PdfReader(pdf_path)
 page_count = 0
+start_time = time.time()
 for i, page in enumerate(reader.pages):
     extracted_text = page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False)
     # grabs out lines
@@ -28,9 +32,9 @@ for i, page in enumerate(reader.pages):
     # year, gender, time, first name, last name, state
     data = [re.split(r' {16,}', info) for info in split_page if len(re.split(r' {16,}', info)) > 5]
     if page_count == 0:
-        headers = [header.replace(' ', '') for header in data[2]]
+        headers = [header.replace(' ', '') for header in data[0]]
         df = pd.DataFrame(columns=headers)
-        data = data[3:]
+        data = data[1:]
         page_count += 1
     # remove excess spaces in names
     clean_data = [clean_spaces(point) for point in data]
@@ -40,7 +44,7 @@ for i, page in enumerate(reader.pages):
         print(e)
         for j, val in enumerate(clean_data):
             if len(val) != 6:
-                logger.info(f'page {i}')
+                logger.info(f'page {j}')
                 logger.info(e)
                 logger.info(split_page[j])
                 logger.info(data[j])
@@ -48,8 +52,11 @@ for i, page in enumerate(reader.pages):
                 logger.info('---')
 
     df = pd.concat([df, df_page])
-    if i % 50 == 0:
+    # progess tracker
+    if i % 200 == 0:
         print(f'On page {i}')
 
-csv_path = "boston_marathon_2.csv" 
-df.to_csv(csv_path, index=False)
+endtime = time.time()
+logger.info(f'took {round(endtime - start_time)} seconds')
+
+df.to_csv(csv_path, index=False, header=True)
